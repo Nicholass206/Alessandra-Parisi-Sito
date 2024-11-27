73c34442df9e1a4ef2e8f98394fd8a10b9d78c28
foodz66 <138889360+foodz66@users.noreply.github.com>
2024-11-27 11:24:58 +0300

    Header Section

---
+++ Alessandra-Parisi-Website/src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+
+
+    return(
+        <div className="HeaderSection">
+            <nav className="HeaderContainer container">
+                <ul className="HeaderList">
+                    <li className="HeaderLink">CHI SONO</li>
+                    <li className="HeaderLink">GALLERIA ICONE</li>
+                    <li className="HeaderLink">CERI PASQUALI</li>
+                    <li className="HeaderLink">COLORI IN ALTRE FORME</li>
+                    <li className="HeaderLink">MOSTRE</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
